4f21d6b60b0e8467e2ec69a6032e82b0f246c437
hahs <alexandre_2420@outlook.com>
2022-09-17 16:50:07 -0500

    added reactive form

--- src/app/products/pages/add/add.component.ts
+++ src/app/products/pages/add/add.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
-  styleUrls: ['./add.component.css']
+  styleUrls: ['./add.component.css'],
 })
 export class AddComponent implements OnInit {
+  myForm = this.fb.group({
+    name: ['', Validators.required],
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  notValid(field: string) {
+    return this.myForm.get(field)?.invalid || false;
   }
-
 }
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
 import { AddComponent } from './pages/add/add.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AddComponent
-  ],
-  imports: [
-    CommonModule,
-    ProductsRoutingModule
-  ]
+  declarations: [AddComponent],
+  imports: [CommonModule, ReactiveFormsModule, ProductsRoutingModule],
 })
-export class ProductsModule { }
+export class ProductsModule {}
